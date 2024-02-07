bcf3f3d9f6b2f2d30236f3e8eb267efbcdb0d179
Christine Campbell <christiecamphoto@gmail.com>
2024-02-07 12:59:18 -0500

    minor updates

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,6 +1,5 @@
 const db = require('./connection');
 const { User, Product, Category } = require('../models');
-const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   await cleanDB('Category', 'categories');
