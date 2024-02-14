dfef7f132e4f61e1e6df9c01371304a13487fa05
Christine Campbell <christiecamphoto@gmail.com>
2024-02-14 03:19:27 -0500

    all components updated to reflect change for redux

--- client/src/components/ProductList/index.jsx
+++ client/src/components/ProductList/index.jsx
@@ -1,20 +1,26 @@
+//react redux dependencies
 import { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { useDispatch, useSelector } from 'react-redux';
+//import apollo client dependencies
 import { useQuery } from '@apollo/client';
+//import utils
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
+//import state management
+import ProductItem from '../ProductItem';
+//import asset
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
   const { currentCategory } = state;
-
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
+  //if data exists or has changed from the response of useQuery, then run dispatch()
   useEffect(() => {
+    //retrieved from server
     if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
@@ -23,6 +29,7 @@ function ProductList() {
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
+      //get cache from idb
     } else if (!loading) {
       idbPromise('products', 'get').then((products) => {
         dispatch({
@@ -33,16 +40,18 @@ function ProductList() {
     }
   }, [data, loading, dispatch]);
 
+  //filter products by category
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
 
+    //return products that match the current category
     return state.products.filter(
       (product) => product.category._id === currentCategory
     );
   }
-
+  
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
