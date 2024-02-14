af25899b7e696fc13b53f0c922dafdc9b8678a46
Christine Campbell <christiecamphoto@gmail.com>
2024-02-14 02:47:50 -0500

    replaced storeprovider from global state with provider from react-redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,22 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
+import { Provider } from 'react-redux';
 import { setContext } from '@apollo/client/link/context';
-
+//import components
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+import store from './utils/store';
 
+//create http link
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+//create auth link
 const authLink = setContext((_, { headers }) => {
+  //get the token from local storage
   const token = localStorage.getItem('id_token');
+  //return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -33,10 +38,10 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <StoreProvider>
+      <Provider store={store}>
         <Nav />
         <Outlet />
-      </StoreProvider>
+      </Provider>
     </ApolloProvider>
   );
 }
