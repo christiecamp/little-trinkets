73f3908639ce71e1b6868ddc6faa5f30934302fe
Christine Campbell <christiecamphoto@gmail.com>
2024-02-15 00:16:12 -0500

    changed issues in seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,5 +1,6 @@
 const db = require('./connection');
 const { User, Product, Category } = require('../models');
+const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   await cleanDB('Category', 'categories');
